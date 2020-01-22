063e823ad56bdd0048526835db4c283b43abc99f
gabriegsti <gabrielgs_2012@hotmail.com>
2020-01-22 13:36:47 -0300

    es6+ todoLists

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-
-
-class Teste{
-    metodo(){
+class List{
+    constructor(){
+        this.data = [];
 
     }
-   outro(){
-
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+class TodoList extends List{
+   constructor(){
+       super();
+       this.usuario = 'Gabriel';
+   }
+   mostraUsuario(){
+       console.log(this.usuario);
    }
 }
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
